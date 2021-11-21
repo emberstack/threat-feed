d0f7e6f3f54ac3558155ab86c998818a4b8aca9f
Romeo Dumitrescu <winromulus@winromulus.com>
2021-11-21 02:49:48 +0200

    Initial commit (#1)
    * Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # threat-feed
-Repository for curated threat sources
+Repository for curated threat sources.
+
+This is a WIP (work in progress) project.
---
+++ src/ES.ThreatFeed/Provider/Configuration/DomainThreatFeedSource.cs
@@ -0,0 +1,11 @@
+﻿namespace ES.ThreatFeed.Provider.Configuration
+{
+    public record DomainThreatFeedSource
+    {
+        public string Url { get; set; }
+        public string Description { get; set; }
+
+        public string Format { get; set; }
+        public string[] Categories { get; set; }
+    }
+}
